03e4dbaa6bd5898db926fd3ac9e93033cb924ea0
anamohammadhur-wq <anamohammadhur@gmail.com>
2026-05-23 21:23:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,154 +1,28 @@
-let coins =
-localStorage.getItem("coins") || 3000;
+let music =
+new Audio("https://cdn.pixabay.com/download/audio/2022/03/15/audio_c8c8a73467.mp3?filename=beautiful-relaxing-music-14437.mp3");
 
-coins = parseInt(coins);
+music.loop = true;
 
-const coinsElement =
-document.getElementById("coins");
-
-const notification =
-document.getElementById("notification");
-
-updateCoins();
-
-function updateCoins(){
-
-coinsElement.innerText = coins;
-
-localStorage.setItem("coins", coins);
-
-}
-
-function showNotification(text){
-
-notification.innerText = text;
-
-notification.style.display = "block";
-
-setTimeout(()=>{
-
-notification.style.display = "none";
-
-},3000);
-
-}
-
-function dailyReward(){
-
-coins += 500;
-
-updateCoins();
-
-showNotification("🎁 حصلت على 500 عملة");
-
-}
+let isPlaying = false;
 
 function toggleMusic(){
 
+if(isPlaying){
+
+music.pause();
+
+showNotification("🔇 تم إيقاف الموسيقى");
+
+isPlaying = false;
+
+}else{
+
+music.play();
+
 showNotification("🎵 تم تشغيل الموسيقى");
 
-}
-
-function buyTheme(price){
-
-if(coins >= price){
-
-coins -= price;
-
-updateCoins();
-
-showNotification("🔥 تم الشراء بنجاح");
-
-}else{
-
-showNotification("❌ لا تملك عملات كافية");
+isPlaying = true;
 
 }
-
-}
-
-function activateTheme(theme){
-
-document.body.classList.remove("neon");
-document.body.classList.remove("blood");
-
-document.body.classList.add(theme);
-
-showNotification("🎨 تم تغيير الثيم");
-
-}
-
-function readManhwa(name){
-
-document.getElementById("reader-title")
-.innerText =
-"📖 جاري قراءة " + name;
-
-window.location.href = "#reader";
-
-showNotification("📚 تم فتح " + name);
-
-}
-
-function login(){
-
-let username =
-prompt("ادخل اسم المستخدم");
-
-if(username){
-
-document.getElementById("username")
-.innerText = username;
-
-document.getElementById("profile-name")
-.innerText = username;
-
-localStorage.setItem("username", username);
-
-showNotification("🔥 أهلاً " + username);
-
-}
-
-}
-
-const savedUser =
-localStorage.getItem("username");
-
-if(savedUser){
-
-document.getElementById("username")
-.innerText = savedUser;
-
-document.getElementById("profile-name")
-.innerText = savedUser;
-
-}
-
-function searchManhwa(){
-
-let input =
-document.getElementById("search")
-.value.toLowerCase();
-
-let cards =
-document.querySelectorAll(".card");
-
-cards.forEach(card => {
-
-let title =
-card.querySelector("h3")
-.innerText.toLowerCase();
-
-if(title.includes(input)){
-
-card.style.display = "block";
-
-}else{
-
-card.style.display = "none";
-
-}
-
-});
 
 }
