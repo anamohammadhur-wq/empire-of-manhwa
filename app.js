b286560550165442d827ebcd42b5f8ba5aed62dc
anamohammadhur-wq <anamohammadhur@gmail.com>
2026-05-23 22:47:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,103 @@
 let coins = 3000;
 
 function updateCoins() {
+  const coinsElement = document.getElementById("coins");
 
-document.getElementById("coins").innerText = coins;
-
+  if (coinsElement) {
+    coinsElement.innerText = coins;
+  }
 }
 
 updateCoins();
 
-function spinWheel() {
+function showNotification(text) {
+  alert(text);
+}
 
-let reward = Math.floor(Math.random() * 1000);
+let music = new Audio(
+  "https://cdn.pixabay.com/download/audio/2022/03/15/audio_c8c8a73467.mp3?filename=beautiful-relaxing-music-14437.mp3"
+);
 
-coins += reward;
+music.loop = true;
 
-updateCoins();
+let isPlaying = false;
 
-alert("🎡 ربحت " + reward + " عملة");
+function toggleMusic() {
+
+  if (isPlaying) {
+
+    music.pause();
+
+    alert("🔇 تم إيقاف الموسيقى");
+
+    isPlaying = false;
+
+  } else {
+
+    music.play();
+
+    alert("🎵 تم تشغيل الموسيقى");
+
+    isPlaying = true;
+
+  }
 
 }
 
 function dailyReward() {
 
-coins += 500;
+  coins += 500;
 
-updateCoins();
+  updateCoins();
 
-alert("🎁 حصلت على 500 عملة");
+  alert("🎁 حصلت على 500 عملة");
+
+}
+
+function spinWheel() {
+
+  let reward = Math.floor(Math.random() * 1000);
+
+  coins += reward;
+
+  updateCoins();
+
+  alert("🎡 ربحت " + reward + " عملة");
 
 }
 
 function login() {
 
-let username = prompt("ادخل اسمك");
+  let username = prompt("ادخل اسمك");
 
-if(username){
+  if (username) {
 
-alert("👑 أهلاً " + username);
+    localStorage.setItem("username", username);
+
+    alert("👑 أهلاً " + username);
+
+  }
 
 }
+
+function readManhwa(name) {
+
+  if (name === "Solo Leveling") {
+
+    window.location.href = "solo.html";
+
+  }
+
+  else if (name === "Nano Machine") {
+
+    window.location.href = "nano.html";
+
+  }
+
+  else {
+
+    alert("📚 قريبًا");
+
+  }
 
 }
