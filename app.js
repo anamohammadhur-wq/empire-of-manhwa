955560672ac2853beaa5411eee9f6f9a059584d1
anamohammadhur-wq <anamohammadhur@gmail.com>
2026-05-23 21:06:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-alert("الجافاسكربت تعمل 🔥");
+function dailyReward() {
+    alert("المكافأة اليومية تعمل 🔥");
+}
+
+function toggleMusic() {
+    alert("الموسيقى تعمل 🎵");
+}
+
+function buyTheme(price) {
+    alert("شراء ناجح بسعر " + price);
+}
+
+function activateTheme(theme) {
+    alert("تم تفعيل " + theme);
+}
+
+function readManhwa(name) {
+    alert("فتح " + name);
+}
