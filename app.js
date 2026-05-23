c1f387a6181d2a718962e4e3c61c3ffe82d07ac5
anamohammadhur-wq <anamohammadhur@gmail.com>
2026-05-23 21:14:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,82 @@
-function dailyReward() {
-    alert("المكافأة اليومية تعمل 🔥");
+let coins = 3000;
+
+const coinsElement =
+document.getElementById("coins");
+
+const notification =
+document.getElementById("notification");
+
+updateCoins();
+
+function updateCoins(){
+
+coinsElement.innerText = coins;
+
 }
 
-function toggleMusic() {
-    alert("الموسيقى تعمل 🎵");
+function showNotification(text){
+
+notification.innerText = text;
+
+notification.style.display = "block";
+
+setTimeout(() => {
+
+notification.style.display = "none";
+
+},3000);
+
 }
 
-function buyTheme(price) {
-    alert("شراء ناجح بسعر " + price);
+function dailyReward(){
+
+coins += 500;
+
+updateCoins();
+
+showNotification("🎁 حصلت على 500 عملة");
+
 }
 
-function activateTheme(theme) {
-    alert("تم تفعيل " + theme);
+function toggleMusic(){
+
+showNotification("🎵 تم تشغيل الموسيقى");
+
 }
 
-function readManhwa(name) {
-    alert("فتح " + name);
+function buyTheme(price){
+
+if(coins >= price){
+
+coins -= price;
+
+updateCoins();
+
+showNotification("🔥 تم الشراء بنجاح");
+
+}else{
+
+showNotification("❌ لا تملك عملات كافية");
+
+}
+
+}
+
+function activateTheme(theme){
+
+document.body.classList.remove("neon");
+document.body.classList.remove("blood");
+
+document.body.classList.add(theme);
+
+showNotification("🎨 تم تغيير الثيم");
+
+}
+
+function readManhwa(name){
+
+showNotification("📖 جاري فتح " + name);
+
+window.location.href = "#reader";
+
 }
