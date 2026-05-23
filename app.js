c36d35299f122e30a014a39958166b9ad4627e8f
anamohammadhur-wq <anamohammadhur@gmail.com>
2026-05-23 22:44:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,131 +1,43 @@
 let coins = 3000;
 
-/* تحديث العملات */
-
 function updateCoins() {
 
-  const coinsElement =
-  document.getElementById("coins");
-
-  if (coinsElement) {
-
-    coinsElement.innerText = coins;
-
-  }
+document.getElementById("coins").innerText = coins;
 
 }
 
 updateCoins();
 
-/* الإشعارات */
+function spinWheel() {
 
-function showNotification(text) {
+let reward = Math.floor(Math.random() * 1000);
 
-  alert(text);
+coins += reward;
+
+updateCoins();
+
+alert("🎡 ربحت " + reward + " عملة");
 
 }
-
-/* مكافأة يومية */
 
 function dailyReward() {
 
-  coins += 500;
+coins += 500;
 
-  updateCoins();
+updateCoins();
 
-  showNotification(
-  "🎁 حصلت على 500 عملة"
-  );
+alert("🎁 حصلت على 500 عملة");
 
 }
-
-/* عجلة الحظ */
-
-function spinWheel() {
-
-  let reward =
-  Math.floor(Math.random() * 1000);
-
-  coins += reward;
-
-  updateCoins();
-
-  showNotification(
-  "🎡 ربحت " + reward + " عملة"
-  );
-
-}
-
-/* تسجيل الدخول */
 
 function login() {
 
-  let username =
-  prompt("👑 ادخل اسمك");
+let username = prompt("ادخل اسمك");
 
-  if (username) {
+if(username){
 
-    localStorage.setItem(
-    "username",
-    username
-    );
-
-    showNotification(
-    "🔥 أهلاً " + username
-    );
-
-    const userElement =
-    document.getElementById("username");
-
-    if (userElement) {
-
-      userElement.innerText =
-      username;
-
-    }
-
-  }
+alert("👑 أهلاً " + username);
 
 }
-
-/* قراءة المانهوا */
-
-function readManhwa(name) {
-
-  if (name === "Solo Leveling") {
-
-    window.location.href =
-    "solo.html";
-
-  }
-
-  else if (name === "Nano Machine") {
-
-    window.location.href =
-    "nano.html";
-
-  }
-
-  else if (name === "Favorites") {
-
-    window.location.href =
-    "favorites.html";
-
-  }
-
-  else if (name === "Profile") {
-
-    window.location.href =
-    "profile.html";
-
-  }
-
-  else {
-
-    showNotification(
-    "📚 قريبًا"
-    );
-
-  }
 
 }
